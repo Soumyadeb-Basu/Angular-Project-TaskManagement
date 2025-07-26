8144939e635992dfe039272fc4b80684345ac52e
Soumyadeb Basu <basusoumyadeb@gmail.com>
2025-07-27 01:37:56 +0530

    Added files.

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, Input, input } from '@angular/core';
+import { DUMMY_USERS } from '../USERS';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  /* selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  image = computed(()=>'assets/users/'+this.selectedUser().avatar);
+*/
+  //@Input({required:true}) avatar!: string;
+  avatar = input.required<string>(); //returns a signal but read-only(cannot be changed by set() method for signals)
+  @Input({ required: true }) name!: string;
+
+  /* get image() {
+    return 'assets/users/' + this.avatar;
+  } */
+
+  image = computed(() => 'assets/users/' + this.avatar());
+
+  onSelectUser() {
+    //const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+    //this.selectedUser[DUMMY_USERS[...]] (Old method without signals, uses zone.js in backend)
+    //this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    //return 'assets/users/' + this.avatar;
+  }
+}
