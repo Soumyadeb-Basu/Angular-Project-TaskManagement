aff1a3960c17ccf26c6354508c31147429c28526
Soumyadeb Basu <basusoumyadeb@gmail.com>
2025-07-29 22:13:19 +0530

    Added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import {DUMMY_USERS} from './USERS' 
+import { TasksComponent } from './tasks/tasks.component';
+import { DUMMY_USERS } from './USERS';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectingUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 import { DUMMY_USERS } from '../USERS';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -16,8 +23,10 @@ export class UserComponent {
   image = computed(()=>'assets/users/'+this.selectedUser().avatar);
 */
   //@Input({required:true}) avatar!: string;
+  @Input({ required: true }) id!: string;
   avatar = input.required<string>(); //returns a signal but read-only(cannot be changed by set() method for signals)
   @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
 
   /* get image() {
     return 'assets/users/' + this.avatar;
@@ -25,10 +34,11 @@ export class UserComponent {
 
   image = computed(() => 'assets/users/' + this.avatar());
 
-  onSelectUser() {
+  onSelectUser(id: string) {
     //const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
     //this.selectedUser[DUMMY_USERS[...]] (Old method without signals, uses zone.js in backend)
     //this.selectedUser.set(DUMMY_USERS[randomIndex]);
     //return 'assets/users/' + this.avatar;
+    this.select.emit(id);
   }
 }
